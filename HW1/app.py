91eb4036eaa1f9a63d2206dfe5091970d686c5d9
이범준 <qjawns281028@gmail.com>
2026-09-21 07:37:47 +0900

    HW1 greeting 추가

--- HW1/app.py
+++ HW1/app.py
@@ -11,3 +11,7 @@ def home():
 def profile():
     hobbies = ["게임 플레이", "노래방", "게임 개발"]
     return render_template("profile.html", hobbies=hobbies)
+
+@app.route("/greet/<name>")
+def greet(name):
+    return render_template("greet.html", name=name)
