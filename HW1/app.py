c945b17fbe090bcde2229621e1008127d3c0fa5e
이범준 <qjawns281028@gmail.com>
2026-09-21 07:32:55 +0900

    HW1 프로필 페이지 추가

--- HW1/app.py
+++ HW1/app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html", name="이범준", student_id="22011669")
+
+@app.route("/profile")
+def profile():
+    hobbies = ["게임 플레이", "노래방", "게임 개발"]
+    return render_template("profile.html", hobbies=hobbies)
